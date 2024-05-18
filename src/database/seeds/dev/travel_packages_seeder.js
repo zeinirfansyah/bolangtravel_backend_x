48d8543a66a0c5d4571de9ebde89cf98df15f553
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-19 06:18:55 +0700

    refactor [*]: fixed typo pacakages to packages

--- src/database/seeds/dev/travel_packages_seeder.js
+++ src/database/seeds/dev/travel_packages_seeder.js
@@ -10,13 +10,13 @@ exports.seed = async function (knex) {
   const travelPackages = [];
   for (let i = 0; i < 24; i++) {
     travelPackages.push({
-      pacakage_category: "family",
-      pacakage_title: faker.lorem.sentence(1),
-      pacakage_location: faker.location.city(),
-      pacakage_duration: faker.number.int({ min: 1, max: 4 }), 
-      pacakage_price: faker.commerce.price(), 
-      pacakage_description: faker.lorem.paragraph(2), 
-      pacakage_thumbnail: faker.image.urlPicsumPhotos(640, 480, true),
+      package_category: faker.helpers.arrayElement(["family", "honeymoon", "solo"]),
+      package_title: faker.music.songName(),
+      package_location: faker.location.city(),
+      package_duration: faker.number.int({ min: 1, max: 4 }), 
+      package_price: faker.commerce.price(), 
+      package_description: faker.lorem.paragraph(2), 
+      package_thumbnail: faker.image.urlPicsumPhotos(640, 480, true),
     });
   }
 
