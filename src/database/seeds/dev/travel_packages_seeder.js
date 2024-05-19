40ea6829aeb57926c9679289e8ae0f4ad1a5496a
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-05-19 21:07:25 +0700

    feat [seeders]: added dummy data for destinations, travel packages, rundowns, and package-destinations

--- src/database/seeds/dev/travel_packages_seeder.js
+++ src/database/seeds/dev/travel_packages_seeder.js
@@ -8,12 +8,12 @@ exports.seed = async function (knex) {
   await knex('travel_packages').del();
 
   const travelPackages = [];
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < 4; i++) {
     travelPackages.push({
       package_category: faker.helpers.arrayElement(["family", "honeymoon", "solo"]),
       package_title: faker.music.songName(),
       package_location: faker.location.city(),
-      package_duration: faker.number.int({ min: 1, max: 4 }), 
+      package_duration: faker.number.int({ min: 1, max: 3 }), 
       package_price: faker.commerce.price(), 
       package_description: faker.lorem.paragraph(2), 
       package_thumbnail: faker.image.urlPicsumPhotos(640, 480, true),
